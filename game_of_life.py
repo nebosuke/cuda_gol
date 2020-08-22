799c46a58953aead5b159bee185500fc6cadb74d
Kensuke Ishida <kensuke.ishida@gmail.com>
2020-08-22 11:36:29 +0900

    時間の計測部分を追加

--- game_of_life.py
+++ game_of_life.py
@@ -2,15 +2,17 @@
 import numpy
 import curses
 from curses import wrapper
+import time
 
 row2str = lambda row: ''.join(['O' if c != 0 else ' ' for c in row])
 cell_value = lambda world, height, width, y, x: world[y % height, x % width]
 
-def print_world(stdscr, world):
+def print_world(stdscr, world, generation, elapsed):
     height, width = world.shape
     for y in range(height):
         row = world[y]
         stdscr.addstr(y, 0, row2str(row))
+    stdscr.addstr(height, 0, "Generation: %06d, Elapsed: %.6f[sec]" % (generation, elapsed / generation), curses.A_REVERSE)
     stdscr.refresh()
 
 def set_next_cell_value(world, next_world, height, width, y, x):
@@ -46,9 +48,15 @@ def game_of_life(stdscr, height, width):
     # 次の世代の世界を保持する２次元配列
     next_world = numpy.empty((height, width), dtype=numpy.int32)
 
+    elapsed = 0.0
+    generation = 0
     while True:
-        print_world(stdscr, world)
+        generation += 1
+        print_world(stdscr, world, generation, elapsed)
+        start_time = time.time()
         calc_next_world_cpu(world, next_world)
+        duration = time.time() - start_time
+        elapsed += duration
         world, next_world = next_world, world
 
 def main(stdscr):
