8bd45fe7ec7b1eea5fc98d68c68b3f4ddec0da40
Kensuke Ishida <kensuke.ishida@gmail.com>
2020-08-22 14:10:22 +0900

    3d cpu version

--- game_of_life.py
+++ game_of_life.py
@@ -6,47 +6,73 @@ import time
 
 row2str = lambda row: ''.join(['O' if c != 0 else ' ' for c in row])
 cell_value = lambda world, height, width, y, x: world[y % height, x % width]
+cell_value_3d = lambda world, depth, height, width, z, y, x: world[z % depth, y % height, x % width]
 
 def print_world(stdscr, world, generation, elapsed):
-    height, width = world.shape
+    depth, height, width = world.shape
+    plane = world[0]
     for y in range(height):
-        row = world[y]
+        row = plane[y]
         stdscr.addstr(y, 0, row2str(row))
     stdscr.addstr(height, 0, "Generation: %06d, Elapsed: %.6f[sec]" % (generation, elapsed / generation), curses.A_REVERSE)
     stdscr.refresh()
 
-def set_next_cell_value(world, next_world, height, width, y, x):
-    current_value = cell_value(world, height, width, y, x)
+def set_next_cell_value(world, next_world, depth, height, width, z, y, x):
+    current_value = cell_value_3d(world, depth, height, width, z, y, x)
     next_value = current_value
     num_live = 0
-    num_live += cell_value(world, height, width, y - 1, x - 1)
-    num_live += cell_value(world, height, width, y - 1, x    )
-    num_live += cell_value(world, height, width, y - 1, x + 1)
-    num_live += cell_value(world, height, width, y    , x - 1)
-    num_live += cell_value(world, height, width, y    , x + 1)
-    num_live += cell_value(world, height, width, y + 1, x - 1)
-    num_live += cell_value(world, height, width, y + 1, x    )
-    num_live += cell_value(world, height, width, y + 1, x + 1)
+
+    num_live += cell_value_3d(world, depth, height, width, z - 1, y - 1, x - 1)
+    num_live += cell_value_3d(world, depth, height, width, z - 1, y - 1, x    )
+    num_live += cell_value_3d(world, depth, height, width, z - 1, y - 1, x + 1)
+    num_live += cell_value_3d(world, depth, height, width, z - 1, y    , x - 1)
+    num_live += cell_value_3d(world, depth, height, width, z - 1, y    , x    )
+    num_live += cell_value_3d(world, depth, height, width, z - 1, y    , x + 1)
+    num_live += cell_value_3d(world, depth, height, width, z - 1, y + 1, x - 1)
+    num_live += cell_value_3d(world, depth, height, width, z - 1, y + 1, x    )
+    num_live += cell_value_3d(world, depth, height, width, z - 1, y + 1, x + 1)
+
+    num_live += cell_value_3d(world, depth, height, width, z    , y - 1, x - 1)
+    num_live += cell_value_3d(world, depth, height, width, z    , y - 1, x    )
+    num_live += cell_value_3d(world, depth, height, width, z    , y - 1, x + 1)
+    num_live += cell_value_3d(world, depth, height, width, z    , y    , x - 1)
+    num_live += cell_value_3d(world, depth, height, width, z    , y    , x + 1)
+    num_live += cell_value_3d(world, depth, height, width, z    , y + 1, x - 1)
+    num_live += cell_value_3d(world, depth, height, width, z    , y + 1, x    )
+    num_live += cell_value_3d(world, depth, height, width, z    , y + 1, x + 1)
+
+    num_live += cell_value_3d(world, depth, height, width, z + 1, y - 1, x - 1)
+    num_live += cell_value_3d(world, depth, height, width, z + 1, y - 1, x    )
+    num_live += cell_value_3d(world, depth, height, width, z + 1, y - 1, x + 1)
+    num_live += cell_value_3d(world, depth, height, width, z + 1, y    , x - 1)
+    num_live += cell_value_3d(world, depth, height, width, z + 1, y    , x    )
+    num_live += cell_value_3d(world, depth, height, width, z + 1, y    , x + 1)
+    num_live += cell_value_3d(world, depth, height, width, z + 1, y + 1, x - 1)
+    num_live += cell_value_3d(world, depth, height, width, z + 1, y + 1, x    )
+    num_live += cell_value_3d(world, depth, height, width, z + 1, y + 1, x + 1)
+
     if current_value == 0 and num_live == 3:
-        next_value = 1
-    elif current_value == 1 and num_live in (2, 3):
-        next_value = 1
+         next_value = 1
+    elif current_value == 1 and num_live in (2, 15):
+         next_value = 1
     else:
-        next_value = 0
-    next_world[y, x] = next_value
+         next_value = 0
 
-def calc_next_world_cpu(world, next_world):
-    height, width = world.shape
-    for y in range(height):
-        for x in range(width):
-            set_next_cell_value(world, next_world, height, width, y, x)
+    next_world[z, y, x] = next_value
 
-def game_of_life(stdscr, height, width):
+def calc_next_3d_world_cpu(world, next_world):
+    depth, height, width = world.shape
+    for z in range(depth):
+        for y in range(height):
+            for x in range(width):
+                set_next_cell_value(world, next_world, depth, height, width, z, y, x)
+
+def game_of_life(stdscr, depth, height, width):
     # 世界の初期値
-    world = numpy.random.randint(2, size=(height, width), dtype=numpy.int32)
+    world = numpy.random.randint(2, size=(depth, height, width), dtype=numpy.int32)
 
-    # 次の世代の世界を保持する２次元配列
-    next_world = numpy.empty((height, width), dtype=numpy.int32)
+    # 次の世代の世界を保持する３次元配列
+    next_world = numpy.empty((depth, height, width), dtype=numpy.int32)
 
     elapsed = 0.0
     generation = 0
@@ -54,7 +80,7 @@ def game_of_life(stdscr, height, width):
         generation += 1
         print_world(stdscr, world, generation, elapsed)
         start_time = time.time()
-        calc_next_world_cpu(world, next_world)
+        calc_next_3d_world_cpu(world, next_world)
         duration = time.time() - start_time
         elapsed += duration
         world, next_world = next_world, world
@@ -63,7 +89,7 @@ def main(stdscr):
     stdscr.clear()
     stdscr.nodelay(True)
     scr_height, scr_width = stdscr.getmaxyx()
-    game_of_life(stdscr, scr_height - 1, scr_width)
+    game_of_life(stdscr, 10, scr_height - 1, scr_width)
 
 if __name__ == '__main__':
     curses.wrapper(main)
